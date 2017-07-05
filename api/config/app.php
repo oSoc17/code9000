7816c0709fb110694e23394ce76ed6ee6aded4af
Demian Dekoninck <demian@live.be>
2017-07-05 12:38:10 +0000

    Apply fixes from StyleCI

--- api/config/app.php
+++ api/config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class
+        Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
@@ -230,7 +230,6 @@ return [
         'View'         => Illuminate\Support\Facades\View::class,
         'JWTAuth'      => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory'   => Tymon\JWTAuth\Facades\JWTFactory::class,
-        
 
     ],
 
