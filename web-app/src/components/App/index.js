0b76237511997d3b00968bb028a2063ec41e710a
Demian Dekoninck <demian@live.be>
2017-07-05 14:45:10 +0200

    redirect to login page when not logged in

--- web-app/src/components/App/index.js
+++ web-app/src/components/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from '../Header';
 import Login from '../Login';
@@ -9,12 +9,24 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // TODO: use a jwt manager
+    const token = window.localStorage.getItem('jwt.token');
+
     return (
       <div className="App">
         <Header />
         <Switch>
-          <Route exact path='/' component={Observations} />
           <Route exact path='/login' component={Login} />
+
+          {token === null 
+            ? <Redirect to="/login"/>
+            : (
+              <div>
+                <Route exact path='/' component={Observations} />
+                <Route exact path='/login' component={Login} />
+              </div>
+            )
+          }
         </Switch>
       </div>
     );
