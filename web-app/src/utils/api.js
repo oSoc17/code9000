76b3ce3fa2e561527ca2ae88a70ad9535846db95
Demian Dekoninck <demian@live.be>
2017-07-05 14:37:41 +0200

    add a api utility function

---
+++ web-app/src/utils/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.REACT_APP_API_URL;
+
+const request = (endpoint, {headers = {}, body, ...otherOptions}, method) => {
+  return axios(`${BASE_URL}${endpoint}`, {
+    ...otherOptions,
+    headers: {
+      ...headers,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    data: body ? JSON.stringify(body) : undefined,
+    method,
+  });
+}
+
+export const api = {
+  get(endpoint, options = {}) {
+    return request(endpoint, options, 'get');
+  },
+  post(endpoint, options = {}) {
+    return request(endpoint, options, 'post');
+  },
+};
+
+export default api;
