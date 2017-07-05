b2478495e310c97e9557c13e446d91857bc57820
bcommein <Bert.Commeine@ugent.be>
2017-07-05 10:13:25 +0200

    start webapp

---
+++ web-app/src/components/picture.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by bert on 5/07/17.
+ */
+import React, { Component } from 'react';
+
+class Picture extends Component{
+    constructor(props){
+        super(props)
+    }
+    render
+}
---
+++ web-app/src/components/votebox.jsx
@@ -0,0 +1,4 @@
+/**
+ * Created by bert on 5/07/17.
+ */
+import React, { Component } from 'react';
