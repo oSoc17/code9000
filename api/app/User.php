e4d5d79d2f353b4037cd158a77c5259bacba76be
Diego De Wilde <diegodw@telenet.be>
2017-07-05 10:53:43 +0200

    add User model

--- api/app/User.php
+++ api/app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the votes for the current user.
+     */
+    public function votes()
+    {
+        return $this->hasMany('App\Vote');
+    }
 }
