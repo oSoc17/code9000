db663c7cd1263a8dfc7bfeaf80cdee69c9b61a5e
Diego De Wilde <diegodw@telenet.be>
2017-07-05 10:54:32 +0200

    add Vote model

---
+++ api/app/Vote.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vote extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable =
+    	'value',
+    ];
+
+    /**
+     * Get the observation where the vote belongs to. 
+     */
+    public function observation()
+    {
+     	return $this->belongsTo('App\Observation');
+    }
+
+    /**
+     * Get the user that did the vote
+     */
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+}
