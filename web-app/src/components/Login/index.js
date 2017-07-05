bfaf9a54090ac6e64cb0352e7ab0bf154c583b33
Demian Dekoninck <demian@live.be>
2017-07-05 12:31:53 +0200

    provide a simple login screen with simple styling

--- web-app/src/components/Login/index.js
+++ web-app/src/components/Login/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Title from '../Title';
+import { Form, Label, Input, Button } from '../Form';
 
-const Login = () => {
-  return (
-    <div>
-      <Title name="Login" />
-      Login
-    </div>
-  );
+import './index.css'
+
+class Login extends Component {
+  constructor(...props) {
+    super(...props);
+
+    this.state = {
+      isValid: false,
+    }
+
+    console.log(this.state.isValid);
+  }
+
+  render() {
+    const { isValid } = this.state;
+
+    return (
+      <div className="Login">
+        <Title name="Login" />
+        <div className="Login__Wrapper">
+          <Form
+            onValidationChange={(valid) => this.setState({ isValid: valid })}
+          >
+            <Label text="Email address" />
+            <Input name="email" rules={['required', 'email']} />
+
+            <Label text="Password" />
+            <Input name="password" type="password" rules={['required']} />
+
+            <Button 
+              disabled={!isValid}
+            >
+              Login
+            </Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
