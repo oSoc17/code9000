4a7e8df18b48cb4e95793a7aebc8a9128274f084
Diego De Wilde <diegodw@telenet.be>
2017-07-05 11:01:55 +0200

    create observations table

---
+++ api/database/migrations/2017_07_05_080807_create_observations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateObservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('observations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('captured_at');
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('latitude', 10, 7);
+            $table->string('picture_storage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('observations');
+    }
+}
