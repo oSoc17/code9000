a9cf6db5cbe39de93184c264966421f7aab00b53
Demian Dekoninck <demian@live.be>
2017-07-05 11:45:45 +0000

    Apply fixes from StyleCI

--- api/app/Http/Controllers/Api/AuthController.php
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
-class AuthController extends Controller {
-    
+class AuthController extends Controller
+{
     public function auth(Request $request)
     {
         $credentials = $request->only('email', 'password');
-    
-        if (! $token = JWTAuth::attempt($credentials)) {
+
+        if (!$token = JWTAuth::attempt($credentials)) {
             return response()->json(['error' => 'invalid_credentials'], 401);
         }
-        
+
         return response()->json(compact('token'));
     }
 }
