cc0509734ae8787c851e5a9a85a29bbf71763454
Diego De Wilde <diegodw@telenet.be>
2017-07-05 12:14:50 +0200

    Add API routes without real implementation

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,3 +18,35 @@ Route::group(['middleware' => 'jwt.auth', 'namespace' => 'Api'], function () {
     Route::post('auth/me', 'AuthController@me');
     Route::post('auth/refresh', 'AuthController@refresh');
 });
+
+Route::group(['middleware' => 'api'], function () {
+
+	/**
+	 * GET
+	 */
+	Route::get('/', function ()    {
+        return 'GET api';
+    });
+    Route::get('observations', function ()    {
+        return 'GET api/observations';
+    });
+    Route::get('observations/{id}', function ($id)    {
+        return 'GET api/observations/' . $id;
+    });
+    Route::get('observations/{id}', function ($id)    {
+        return 'GET api/observations/' . $id;
+    });
+    Route::get('observations/{id}/picture', function ($id)    {
+        return 'GET api/observations/' . $id . '/picture';
+    });
+
+    /**
+	 * POST
+	 */
+    Route::post('observations', function ()    {
+        return 'POST api/observations';
+    });
+    Route::post('observations/vote', function ()    {
+        return 'POST api/observations/vote';
+    });
+});
