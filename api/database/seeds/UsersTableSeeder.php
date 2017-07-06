4fb0725eb05edf233292a37f255ddc72d6725466
Diego De Wilde <diegodw@telenet.be>
2017-07-06 11:40:46 +0200

    add oSoc17 logins

--- api/database/seeds/UsersTableSeeder.php
+++ api/database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $this->seedDevelopLogin();
+        $this->seedOsocLogins();
     }
 
     /**
@@ -22,6 +23,20 @@ class UsersTableSeeder extends Seeder
     {
     	$this->createUser('develop', 'develop@bird.today', 'bird');
     }
+
+	/**
+     * Add all oSoc17 accounts
+     */
+    private function seedOsocLogins()
+    {
+    	// If there was a spacebar in the first name it was removed. Special characters such as 'ë','ç' and dashes '-' are used.
+		$firstnames = ['dylan','demian','bert','diëgo','cynthia','krishnakumar','tim','françois','redouane','eveline','pierre-alexandre','niels','eva','pol','dieter','damian','moustapha','arnaud','theo','alexandra','ismail','maxime','remco','sheila','astrid','brysen','elias','pieter-jan','jasper','robrecht','israelketema','bruno','michiel','thor','jean-pacifique','simon','ruben','thibault','arne','frederik','ben','pieter','johan','xavier','miet','jonathan','julian','dries'];
+		
+		// Create one user account per person
+		foreach ($firstnames as $firstname) {
+			$this->createUser($firstname, $firstname.'@bird.today', 'code9000');
+		}
+	}
 
 	/**
 	 * Create User with name, email and password
