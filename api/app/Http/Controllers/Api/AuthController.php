092bf7c9348244845410d2fc2394536a6e2093c9
Demian Dekoninck <demian@live.be>
2017-07-10 10:27:04 +0200

    add some documentation to endpoint actions

--- api/app/Http/Controllers/Api/AuthController.php
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,15 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
+    
+    /**
+     * Authenticate the user and create a token
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     */
     public function auth(Request $request)
     {
         $credentials = $request->only('email', 'password');
@@ -18,14 +27,24 @@ class AuthController extends Controller
 
         return response()->json(compact('token'));
     }
-
+    
+    /**
+     * Return the current authenticated user
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function me(Request $request)
     {
-        dd(auth()->user());
-
         return response()->json(auth()->user());
     }
-
+    
+    /**
+     * Refresh the JWT token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function refresh()
     {
         $token = JWTAuth::getToken();
--- api/app/Http/Controllers/Api/ObservationController.php
+++ api/app/Http/Controllers/Api/ObservationController.php
@@ -9,18 +9,38 @@ use App\Http\Requests\Api\ObservationModel;
 
 class ObservationController extends Controller
 {
+    
+    /**
+     * Return all observations
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function index()
     {
         return Observation::all();
     }
-
+    
+    /**
+     * Get the observation metadata with an id
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
     public function show($id)
     {
         $observation = Observation::find($id);
 
         return $observation->toJson();
     }
-
+    
+    /**
+     * Return the picture that is stored
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
     public function getPicture($id)
     {
         $observation = Observation::find($id);
@@ -28,7 +48,14 @@ class ObservationController extends Controller
 
         return response($image)->header('Content-Type', 'image/jpeg');
     }
-
+    
+    /**
+     * Create a new observation
+     *
+     * @param \App\Http\Requests\Api\ObservationModel $request
+     *
+     * @return mixed
+     */
     public function store(ObservationModel $request)
     {
         $file = $request->file('image');
