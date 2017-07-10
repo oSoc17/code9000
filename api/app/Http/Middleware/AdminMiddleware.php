3f86cb9e46ac1185c7247d5fb631861185c494e3
Demian Dekoninck <demian@live.be>
2017-07-10 15:06:57 +0200

    add AdminMiddleware

---
+++ api/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware {
+    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check() && auth()->user()->isAdmin())
+        {
+            return $next($request);
+        }
+        
+        return response()->json('You are not authenticated or access is denied for your account.', 401);
+    }
+}
