5015f18044e7ed304910e0dbb7744ea62c1024e4
Demian Dekoninck <demian@live.be>
2017-07-10 09:19:23 +0000

    Apply fixes from StyleCI

--- api/app/Http/Controllers/Api/GithubWebhookController.php
+++ api/app/Http/Controllers/Api/GithubWebhookController.php
@@ -11,9 +11,9 @@ class GithubWebhookController extends Controller
     public function deploy(Request $request)
     {
         $ref = sprintf('refs/heads/%s', config('app.deploy_branch_webhook'));
-    
+
         $payload = json_decode($request->payload);
-            
+
         abort_unless($ref === $payload->ref, 403);
 
         $response = Artisan::call('deploy');
