77b4c2c358ceacad4cf0092f130088148e9b0418
Demian Dekoninck <demian@live.be>
2017-07-11 14:29:25 +0200

    update reducer for observations

--- web-app/src/reducers/index.js
+++ web-app/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import application from './application';
+import observations from './observations';
 
 const rootReducer = combineReducers({
   application,
+  observations,
 });
 
 export default rootReducer;
