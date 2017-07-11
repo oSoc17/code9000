4a710c54e6159b19214fb85e78c914b4a73196d5
Diego De Wilde <diegodw@telenet.be>
2017-07-11 15:46:23 +0200

    create and implement PasswordReset model

--- api/app/PasswordReset.php
+++ api/app/PasswordReset.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordReset extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email',  
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'email', 'token', 'created_at',
+    ];
 }
