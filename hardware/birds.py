b896d2ee19ec862f877d4f14cd942cecd49d8d8f
Dylan Van Assche <dylan.van.assche@protonmail.com>
2017-07-11 15:33:16 +0200

    Recommiting

--- hardware/birds.py
+++ hardware/birds.py
@@ -2,15 +2,23 @@
     Title: birds.py
     Description: Take pictures when a birds is passing by and send it to the API.
     License: see Github project: https://www.github.com/oSoc17/code9000
+    Depends: python-requests, python-picamera
 """
 
 # Python built-in modules
 import logging
 import json
+import sys
 from time import gmtime, strftime, sleep
+import threading
+import os
+import asyncio
+
+# Set current working directory
+os.chdir(sys.path[0])
 
 # Set logging level
-logging.setLevel(logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(message)s')
 
 # Python external modules
 try:
@@ -33,43 +41,105 @@ configData = configFile.read()
 configFile.close()
 configData = json.loads(configData)
 
+# Create directories if not exist
+if not os.path.exists(configData['pictureDir']):
+	os.makedirs(configData['pictureDir'])
+
 # Create a camera instance
 camera = picamera.PiCamera()
+
+def takePicture(channel):
+    global camera
+    global uploadNow
+    
+    logging.debug('Bird detected!')
+    logging.debug('Getting current time...')
+    currentTimestamp = getTime() # Read the current time
+    logging.debug('Taking picture...')
+    camera.capture('{}/{}.jpg'.format(configData['pictureDir'], currentTimestamp)) # Take picture
+
+    # Too much pictures on the SD card, start uploading directly
+    if len(os.listdir(configData['pictureDir'])) > configData['maxpictures'] and not uploadTimer.is_alive() and not uploadNow:	
+        logging.info('Picture batch too large, initiate upload now...')
+        uploadNow = True
+
+    logging.debug('Bird event completed!')
+    
 
 # Setup GPIO pins
 GPIO.setmode(GPIO.BOARD) # Use BOARD GPIO numbers
 GPIO.setup(configData['pirsensor'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Setup GPIO for PIR sensor
-GPIO.add_event_detect(configData['pirsensor'], GPIO.FALLING, bouncetime=configData['bouncetime'])
+GPIO.add_event_detect(configData['pirsensor'], GPIO.FALLING, bouncetime=configData['bouncetime'], callback=takePicture)
+
+uploadNow = False
+
+# Upload timer callback
+def upload():
+	global uploadNow
+	uploadNow = True
+
+networkBuffer = 0 # Limit max requests to avoid overloading the network card
+
+@asyncio.coroutine
+def uploadAsync():
+	asyncLoop = asyncio.get_event_loop()
+	global uploadNow
+
+	def _executeReq(f):
+		global networkBuffer
+		if networkBuffer < configData['networkBuffer']:
+			networkBuffer = networkBuffer + 1
+			currentFile = open('{}/{}'.format(configData['pictureDir'], f), 'rb')
+			currentUpload = {'image': currentFile} # Read file
+			currentTimestamp = f.split('.')[0]
+			try:
+				r = requests.post(configData['api'], data={'longitude': configData['location']['lon'], 'latitude': configData['location']['lat'], 'captured_at': currentTimestamp, 'token': configData['token'] }, files=currentUpload) # Upload data to API
+			except requests.exceptions.ConnectionError as error:
+				logging.error('Suddenly, a wild network error appeared: {}'.format(error))
+			except:
+				logging.error('An error occured while uploading data to the API')
+			finally:
+				if r.status_code == 200:
+					os.remove('{}/{}'.format(configData['pictureDir'], f))
+					networkBuffer = networkBuffer - 1 # Request complete
+					logging.debug('Uploading data OK')
+	
+			return currentFile.close() # Close file
+		else:
+			return logging.debug('Network buffer is full!')
+
+
+	if uploadNow and len(os.listdir(configData['pictureDir'])):
+		logging.debug('Uploading asynchronous the picture and meta data to API')
+		req = asyncLoop.run_in_executor(None, _executeReq, os.listdir(configData['pictureDir'])[0])
+
+uploadTimer = threading.Timer(configData['timebetween'], upload)
+uploadTimer.start()
+
+def getTime():
+	return strftime('%Y-%m-%d %H:%M:%S', gmtime())
 
 def loop():
     try:
-        print('You can always exit this program by pressing CTRL + C')
+        logging.info('You can always exit this program by pressing CTRL + C')
         logging.info('Looking for birds...')
-
-        while True:
-            # Bird detected, take a picture
-            if GPIO.event_detected(configData['pirsensor']):
-                logging.debug('Bird detected!')
-                GPIO.remove_event_detect(configData['pirsensor']) # Disable interrupt
-                logging.debug('Getting current time...')
-                currentTime = getTime() # Read the current time
-                logging.debug('Taking picture...')
-                camera.capture('{}.jpg'.format(currentTime)) # Take picture
-                GPIO.add_event_detect(configData['pirsensor'], GPIO.FALLING, bouncetime=configData['bouncetime']) # Enabled interrupt again
-                logging.debug('Bird event completed!')
-
-            # Nothing happened, sleep further
-            else:
-                sleep(configData['sleeptime'])
+        while(True):
+            asyncLoop = asyncio.get_event_loop()
+            asyncLoop.run_until_complete(uploadAsync())
+            sleep(configData['sleeptime'])
 
     except KeyboardInterrupt:
+        logging.debug('Shutting down and cleaning up...')
+        uploadTimer.join()
         GPIO.cleanup()
-        logging.info('I stopped with looking for birds!')
+        logging.info('Shutdown complete!')
 
 # Start the loop()
 # Reason for the IF statement is here: https://stackoverflow.com/questions/419163/what-does-if-name-main-do
-if __name__ == "__main__":
+if __name__ == '__main__':
     loop()
 
-# Clean up in case something is failing
+# Clean up too when crashing...
+logging.error('Shutdown and cleaning up after crash...')
+uploadTimer.join()
 GPIO.cleanup()
