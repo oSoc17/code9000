3ef36f194e3964bf448e94a5dabf3ba9915af013
Demian Dekoninck <demian@live.be>
2017-07-11 14:28:21 +0200

    fix rebase

--- api/routes/api.php
+++ api/routes/api.php
@@ -27,6 +27,8 @@ Route::group(['namespace' => 'Api'], function () {
             Route::post('me', 'AuthController@me');
             Route::post('refresh', 'AuthController@refresh');
             Route::post('logout', 'AuthController@logout');
+            
+            Route::get('observations', 'ObservationController@forUser');
         });
 
         Route::post('votes', 'VotesController@store');
