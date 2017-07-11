dfff37af0f77bc2d46f8eae8872ff95efb1bbe84
Diego De Wilde <diegodw@telenet.be>
2017-07-11 15:37:57 +0200

    Add comment

--- api/app/Installation.php
+++ api/app/Installation.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Installation extends Model
 {
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'active',
         'token',
