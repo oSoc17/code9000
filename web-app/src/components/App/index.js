3645d3f23f65f73297511831e79772503bea9f64
Demian Dekoninck <demian@live.be>
2017-07-11 13:22:25 +0200

    connect the App to the state

--- web-app/src/components/App/index.js
+++ web-app/src/components/App/index.js
@@ -1,34 +1,15 @@
-/* global window */
-import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
-import Header from '../Header';
-import Login from '../Login';
-import Observations from '../Observations';
+import App from './App';
 
-import './App.css';
+import { isLoading } from '../../selectors';
 
-class App extends Component {
-  render() {
-    // TODO: use a jwt manager
-    const token = window.localStorage.getItem('jwt.token');
+const mapStateToProps = (state) => ({
+  loading: isLoading(state),
+});
 
-    return (
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/login" component={Login} />
-
-          {token === null
-            ? <Redirect to="/login" />
-            : <div>
-              <Route exact path="/" component={Observations} />
-              <Route exact path="/login" component={Login} />
-            </div>}
-        </Switch>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default withRouter(connect(
+  mapStateToProps,
+  undefined,
+)(App));
