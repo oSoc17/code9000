cd5a5cfa7fe279d151a30998e23305547ed696c1
Demian Dekoninck <demian@live.be>
2017-07-11 14:28:38 +0200

    load observations of user

--- web-app/src/components/Bootstrap/BootstrapContainer.js
+++ web-app/src/components/Bootstrap/BootstrapContainer.js
@@ -7,7 +7,7 @@ class BootstrapContainer extends Component {
     const { loadObservations, finishInitialLoading } = this.props;
 
     Promise.all([
-      api.get('/observations').then(({ data: paginationModel }) => loadObservations(paginationModel.data)),
+      api.get('/auth/observations').then(({ data: paginationModel }) => loadObservations(paginationModel.data)),
     ])
       .then(finishInitialLoading);
   }
