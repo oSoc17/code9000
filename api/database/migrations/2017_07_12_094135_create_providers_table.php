4863ef16d0f639159d5af276345abf4fb4c1d8f7
Demian Dekoninck <demian@live.be>
2017-07-12 12:02:38 +0200

    add migration for Provider table

---
+++ api/database/migrations/2017_07_12_094135_create_providers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('provider');
+            $table->string('provider_id');
+            $table->string('provider_token', 512);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
