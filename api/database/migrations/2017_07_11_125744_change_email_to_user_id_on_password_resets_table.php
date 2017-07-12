04d206a1c292bfe75b0571f02282955e91e37043
Diego De Wilde <diegodw@telenet.be>
2017-07-12 13:11:23 +0200

    rename migration (wrong table)

--- api/database/migrations/2017_07_11_125744_change_email_to_user_id_on_password_resets_table.php
+++ api/database/migrations/2017_07_11_125744_change_email_to_user_id_on_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeEmailToUserIdOnUsersTable extends Migration
+class ChangeEmailToUserIdOnPasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class ChangeEmailToUserIdOnUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('password_resets', function (Blueprint $table) {
             $table->dropColumn('email');
         });
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('password_resets', function (Blueprint $table) {
             $table->integer('user_id')->index();
         });
     }
@@ -28,10 +28,10 @@ class ChangeEmailToUserIdOnUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('password_resets', function (Blueprint $table) {
             $table->dropColumn('user_id');
         });
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
         });
     }
