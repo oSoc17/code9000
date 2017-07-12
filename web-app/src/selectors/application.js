bc86660978c257347329c631bf61d05440a46293
Demian Dekoninck <demian@live.be>
2017-07-12 14:34:33 +0200

    add getUser and isAdmin selectors

--- web-app/src/selectors/application.js
+++ web-app/src/selectors/application.js
@@ -1,1 +1,3 @@
 export const isLoading = (state) => state.application.loading;
+export const getUser = (state) => state.application.user;
+export const isAdmin = (state) => state.application.user && state.application.user.is_admin === 1;
