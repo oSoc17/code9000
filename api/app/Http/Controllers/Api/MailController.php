ead4ece53a06b5f27c8716bb38635715004f1f6c
Diego De Wilde <diegodw@telenet.be>
2017-07-13 07:15:32 +0200

    implement MailController

--- api/app/Http/Controllers/Api/MailController.php
+++ api/app/Http/Controllers/Api/MailController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Mail;
 
 class MailController extends Controller
 {
-    //
+    /**
+     * Show the application sendMail.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendPasswordResetMail($receiverAddress)
+    {
+    	$content = [
+    		'title'=> 'Reset your password', 
+    		'body'=> 'The body of your message.',
+    		'button' => 'Reset your password'
+    		];
+
+    	Mail::to($receiverAddress)->send(new PasswordReset($content));
+    }
 }
