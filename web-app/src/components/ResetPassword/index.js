dccdace0dcdd3ac4a5cf24bd759fabf5e718c152
Demian Dekoninck <demian@live.be>
2017-07-13 13:48:42 +0200

    add reset password screen

---
+++ web-app/src/components/ResetPassword/index.js
@@ -0,0 +1,40 @@
+/* global window */
+import React, { Component } from 'react';
+
+import Title from '../Title';
+import GuestMode from '../GuestMode';
+import { Form, Input, Button }  from '../Form';
+import api from '../../utils/api';
+
+import './ResetPassword.css';
+
+class ResetPassword extends Component {
+  constructor(...props) {
+    super(...props);
+
+    this.state = {
+      isValid: false,
+    };
+  }
+
+
+  render() {
+    const { isValid } = this.state;
+
+    return (
+      <GuestMode className="ResetPassword">
+        <Title name="Reset Password" />
+        <Form
+          onValidationChange={(valid) => this.setState({ isValid: valid })}
+        >
+          <Input name="email" rules={['required', 'email']} placeholder="Email" />
+          <div className="ResetPassword__Button">
+            <Button disabled={!isValid}>Reset</Button>
+          </div>
+        </Form>
+      </GuestMode>
+    );
+  }
+}
+
+export default ResetPassword;
