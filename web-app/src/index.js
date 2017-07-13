77bd3021547ab412bd6fcb9f7b7a365bf04c8ce8
Demian Dekoninck <demian@live.be>
2017-07-13 16:26:14 +0200

    show the NotFound component if page is not found

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -14,6 +14,7 @@ import LoginCallback from './components/Login/LoginCallback';
 import StartScreen from './components/StartScreen';
 import ResetPassword from './components/ResetPassword';
 import SignUp from './components/SignUp';
+import NotFound from './components/NotFound';
 
 import './index.css';
 
@@ -54,6 +55,7 @@ const Root = () => (
         <Route exact path="/sign-up" component={SignUp} />
 
         {isAuthenticated()}
+        <Route component={NotFound} />
       </Switch>
     </Router>
   </Provider>
