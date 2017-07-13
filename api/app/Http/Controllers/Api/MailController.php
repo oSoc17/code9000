20028db31ff1eb6f803383ca64279ad8abc124fe
Diego De Wilde <diegodw@telenet.be>
2017-07-13 12:27:04 +0200

    add html line break tags

--- api/app/Http/Controllers/Api/MailController.php
+++ api/app/Http/Controllers/Api/MailController.php
@@ -17,8 +17,8 @@ class MailController extends Controller
     {
         $content = [
             'title'=> 'Reset your password',
-            'body'=> 'Dear '.$request->name.', please click the link below to reset your password. If you have not requested a new password, please ignore this email.',
-            'button' => 'Reset your password',
+            'body'=> 'Dear '.$request->name.', </br> Please click the link below to reset your password. </br> If you have not requested a new password, please ignore this email.',
+            'button' => 'Reset your password!',
             'url' => $request->url,
         ];
         Mail::to($request->email)->send(new PasswordReset($content));
