a555a6ea060be68b674f028248146e53966ee571
Diego De Wilde <diegodw@telenet.be>
2017-07-15 18:18:01 +0200

    implement resetPassword function and add consistency

--- api/app/Http/Controllers/Api/AuthController.php
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -11,6 +11,7 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\UserLogin;
 use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use App\Http\Requests\Api\NewPasswordModel;
 use App\Http\Requests\Api\PasswordResetModel;
 use App\Http\Requests\Api\UserRegistrationModel;
 
@@ -118,8 +119,8 @@ class AuthController extends MailController
     {
         $user_email = $request->email;
         $user = User::where('email', $user_email)->first();
-        if ($user && $this->notSpamming($user, 30)) {
-            // User exists and had no request < 30m
+        if ($user && $this->notSpamming($user, env("PASSWORD_RESET_MINUTES", 30))) {
+            // User exists and had no request < PASSWORD_RESET_MINUTES
             $uuid_token = Uuid::generate(4);
             $url = url('/reset/'.$uuid_token);
             $request->request->add(['url' => $url]);
@@ -144,13 +145,35 @@ class AuthController extends MailController
                 return true;
             }
             $last_password_request_time = $last_password_request->created_at;
-            $now_DateTime = new DateTime();
-            $last_password_request_DateTime = new DateTime($last_password_request_time);
-            $interval = $now_DateTime->diff($last_password_request_DateTime)->i;
-            if($interval >= $minutes) {
-                return true;
-            }
+            $this->isInsideInterval($last_password_request_time, $minutes);
         }
         return false;
+    }
+
+    private function isInsideInterval($lastTime, $minutes)
+    {
+        $now_DateTime = new DateTime();
+        $last_DateTime = new DateTime($lastTime);
+        $interval = $now_DateTime->diff($last_DateTime)->i;
+        if($interval >= $minutes) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Store the new password in the database if token is valid and time < PASSWORD_RESET_MINUTES.
+     *
+     * @param \App\Http\Requests\Api\NewPasswordModel $request
+     * @param string $token
+     */
+    public function resetPassword(NewPasswordModel $request, $token)
+    {
+        $password_reset = PasswordReset::where('token', $token)->first();
+        if($password_reset && $this->isInsideInterval($password_reset->created_at, env("PASSWORD_RESET_MINUTES", 30))) {
+            $user = User::find($password_reset->user_id);
+            $user->password = bcrypt($request->password);
+            $user->save();
+        }
     }
 }
