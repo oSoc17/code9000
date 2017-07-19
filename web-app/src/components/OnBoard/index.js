c504d75640965df9530f358b087b9558429f00df
Demian Dekoninck <demian@live.be>
2017-07-20 00:23:29 +0200

    make it less hard coded

--- web-app/src/components/OnBoard/index.js
+++ web-app/src/components/OnBoard/index.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import Slider from './components/Slider';
 
+import classNames from '../../utils/classNames';
+
 import Header from '../Header';
 import './OnBoard.css';
 
@@ -10,6 +12,12 @@ import armIcon from '../../theme/icons/arm.svg';
 import verkijkerIcon from '../../theme/icons/verkijker.svg';
 import polaroidIcon from '../../theme/icons/polaroid.svg';
 
+const PROGRESS_ITEM = 0.25;
+const PROGRESS_LAST = PROGRESS_ITEM * 3;
+const PROGRESS_SECOND_LAST = PROGRESS_ITEM * 2;
+const TIME_ANIMATION = 5000;
+const WIDTH_BERT = 325;
+
 class OnBoard extends Component {
   constructor(...props) {
     super(...props);
@@ -17,66 +25,48 @@ class OnBoard extends Component {
     this.state = {
       showFixedPolaroid: false,
       progress: undefined,
-      top: undefined,
-      bottom: undefined,
-      armBottom: 'visibile',
-      currentSlide: 0,
+      disappear: false,
+      next: false,
     };
   }
 
   checkProgress(percent) {
+    const disappear = percent >= PROGRESS_LAST;
+
+    if (disappear) {
+      this.next();
+    }
+
     this.setState({
-      showFixedPolaroid: percent >= 0.5,
+      showFixedPolaroid: percent >= PROGRESS_SECOND_LAST,
       progress: percent,
-      top: this.calculateTopPolaroid(),
-      bottom: this.calculateBottomBert(),
-      armBottom: this.calculateBottomArm(),
-    });
+      disappear,
+    }, () => this.styleArm());
   }
 
-  checkIndex(index) {
-    this.setState({
-      currentSlide: index,
-    });
+  next() {
+    setTimeout(() => {
+      if (this.state.disappear) {
+        this.setState({ next: true });
+      }
+    }, TIME_ANIMATION);
   }
 
-  calculateTopPolaroid() {
-    const { progress } = this.state;
-
-    const top = (window.innerHeight - (152 + 127)); // Margin from bottom + height of polaroid
-
-    if (progress >= 0.5) {
-      return top * (1 - ((progress - 0.5) * 4.15));
-    }
-
-    return top - (60 - 5); // Height of header
-  }
-
-  calculateBottomBert() {
-    const { progress } = this.state;
-
-    if (progress >= 0.5) {
-      return -20 * (1 + ((progress - 0.5) * 72));
-    }
-
-    return -20;
-  }
-
-  calculateBottomArm() {
+  styleArm() {
     const { progress } = this.state;
 
     return {
-      visibility: progress > 0.5 ? 'hidden' : 'visible',
+      visibility: window.innerHeight < WIDTH_BERT || progress > PROGRESS_SECOND_LAST ? 'hidden' : 'visible',
       zIndex: progress >= 0.04 && progress < 0.34 ? '0' : '1',
     };
   }
 
   render() {
-    const { showFixedPolaroid, currentSlide } = this.state;
+    const { showFixedPolaroid, disappear, next } = this.state;
 
-    if (currentSlide >= 3) {
+    if (next) {
       return (
-        <div>Start trial</div>
+        <div>Are you ready for the next step?</div>
       );
     }
 
@@ -85,44 +75,70 @@ class OnBoard extends Component {
         <Header />
         <div className="OnBoard__Content" />
 
-        <div className="OnBoard__Footer">
-          <img src={bertIcon} alt="Avatar of Bert, the Bird nerd." className="OnBoard__Footer__Bert" style={{ bottom: `${this.calculateBottomBert()}px` }} />
-          <img src={armIcon} alt="Arm of Bert." className="OnBoard__Footer__Arm" style={this.calculateBottomArm()} />
-          {showFixedPolaroid && (<img src={polaroidIcon} alt="" className="OnBoard__Carrousel__Polaroid OnBoard__Carrousel__PolaroidFixed" style={{ top: `${this.calculateTopPolaroid()}px` }} />)}
+        <div className="OnBoard__Bottom">
+          <img
+            src={bertIcon}
+            alt="Avatar of Bert, the Bird nerd."
+            className={classNames('OnBoard__Bert', disappear && 'OnBoard__Bert--disappear')}
+          />
+
+          <img
+            src={armIcon}
+            alt="Arm of Bert."
+            className="OnBoard__Arm"
+            style={this.styleArm()}
+          />
+
+          {showFixedPolaroid && (
+            <img
+              src={polaroidIcon}
+              alt=""
+              className={classNames('Carrousel__Polaroid', disappear && 'Carrousel__Polaroid--disappear')}
+            />
+          )}
 
           <Slider
-            className="OnBoard__Carrousel"
+            className="Carrousel"
             process={(percent) => this.checkProgress(percent)}
-            currentIndex={(index) => this.checkIndex(index)}
           >
-            <div className="OnBoard__Carrousel__Item">
-              <div className="OnBoard__Carrousel__Item__Text">Hi, my name is Bert.</div>
-              <div className="OnBoard__Carrousel__Item__Text">I LOVE birds. And I need your help.</div>
-              <img src={verkijkerIcon} alt="" className="OnBoard__Carrousel__Glass" />
-            </div>
-            <div className="OnBoard__Carrousel__Item">
-              <div className="OnBoard__Carrousel__Item__Text">I&apos;m looking for the common tern.</div>
-              <div className="OnBoard__Carrousel__Item__Text">I took some pictures, this is what they look like!</div>
 
-              <div className="OnBoard__Carrousel__Dias">
-                <div className="OnBoard__Carrousel__Dia OnBoard__Carrousel__Dia--1">
+            <div className="Carrousel__Item">
+              <div className="Carrousel__Item__Text">Hi, my name is Bert.</div>
+              <div className="Carrousel__Item__Text">I LOVE birds. And I need your help.</div>
+              <img src={verkijkerIcon} alt="" className="Carrousel__Glass" />
+            </div>
+
+            <div className="Carrousel__Item">
+              <div className="Carrousel__Item__Text">I&apos;m looking for the common tern.</div>
+              <div className="Carrousel__Item__Text">I took some pictures, this is what they look like!</div>
+              <div className="Carrousel__Dias">
+                <div className="Carrousel__Dia Carrousel__Dia--1">
                   <img src="" alt="" />
                 </div>
-                <div className="OnBoard__Carrousel__Dia OnBoard__Carrousel__Dia--2">
+                <div className="Carrousel__Dia Carrousel__Dia--2">
                   <img src="" alt="" />
                 </div>
-                <div className="OnBoard__Carrousel__Dia OnBoard__Carrousel__Dia--3">
+                <div className="Carrousel__Dia Carrousel__Dia--3">
                   <img src="" alt="" />
                 </div>
               </div>
             </div>
-            <div className="OnBoard__Carrousel__Item">
-              <div className="OnBoard__Carrousel__Item__Text">I&apos;m going to the HoutDok in Ghent right now to take some more pictures..</div>
-              <div className="OnBoard__Carrousel__Item__Text">I would like you to help me spot the common tern.</div>
 
-              {!showFixedPolaroid && <img src={polaroidIcon} alt="" className="OnBoard__Carrousel__Polaroid" style={{ top: `${this.calculateTopPolaroid()}px` }} />}
+            <div className="Carrousel__Item">
+              <div className="Carrousel__Item__Text">I&apos;m going to the HoutDok in Ghent right now to take some more pictures..</div>
+              <div className="Carrousel__Item__Text">I would like you to help me spot the common tern.</div>
+
+              {!showFixedPolaroid && (
+                <img
+                  src={polaroidIcon}
+                  alt=""
+                  className="Carrousel__Polaroid"
+                />
+              )}
             </div>
-            <div className="OnBoard__Carrousel__Item" />
+
+            <div className="Carrousel__Item" />
+
           </Slider>
         </div>
       </div>
