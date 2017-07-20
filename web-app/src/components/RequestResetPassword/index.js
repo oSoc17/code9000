8933665ea2c7ca10491cf0c0cf9af8f4ebc91721
Demian Dekoninck <demian@live.be>
2017-07-20 13:05:26 +0200

    show Success message when password request is success

--- web-app/src/components/RequestResetPassword/index.js
+++ web-app/src/components/RequestResetPassword/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Title from '../Title';
 import GuestMode, { GoBack } from '../GuestMode';
 import { Form, Input, Button } from '../Form';
+import { Success } from '../Alerts';
 
 import api from '../../utils/api';
 
@@ -18,28 +19,33 @@ class RequestResetPassword extends Component {
     };
   }
 
-  requestResetPassword({ body }) {
+  requestResetPassword(body) {
     api.post('/auth/reset', body).then(() => {
       this.setState({ success: true });
     });
   }
 
   render() {
-    const { isValid } = this.state;
+    const { isValid, success } = this.state;
 
     return (
       <GuestMode className="RequestResetPassword">
         <Title name="Reset Password" />
-        <div className="GuestMode__Label">To reset, please fill in your email:</div>
-        <Form
-          onValidationChange={(valid) => this.setState({ isValid: valid })}
-          onSubmit={(body) => this.requestResetPassword(body)}
-        >
-          <Input name="email" rules={['required', 'email']} placeholder="Email" className="RequestResetPassword__Input" />
-          <div className="RequestResetPassword__Button">
-            <Button disabled={!isValid}>Reset password</Button>
-          </div>
-        </Form>
+
+        {success && <Success body="We have sent you an e-mail to reset your password." />}
+
+        {!success && (
+          <Form
+            onValidationChange={(valid) => this.setState({ isValid: valid })}
+            onSubmit={(body) => this.requestResetPassword(body)}
+          >
+            <div className="GuestMode__Label">To reset, please fill in your email:</div>
+            <Input name="email" rules={['required', 'email']} placeholder="Email" className="RequestResetPassword__Input" />
+            <div className="RequestResetPassword__Button">
+              <Button disabled={!isValid}>Reset password</Button>
+            </div>
+          </Form>
+        )}
         <GoBack />
       </GuestMode>
     );
