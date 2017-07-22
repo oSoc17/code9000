51fd623fdb1d022829618d06370cd167f87e0514
Demian Dekoninck <demian@live.be>
2017-07-22 15:30:07 +0200

    extra the generation of image url to seperate function

--- web-app/src/components/Observations/Observations.js
+++ web-app/src/components/Observations/Observations.js
@@ -8,6 +8,10 @@ import polaroid from '../../theme/icons/polaroid.svg';
 import trash from '../../theme/icons/trash.svg';
 import book from '../../theme/icons/book.svg';
 
+const generateImageUrl = (observationId) => {
+  return `${process.env.REACT_APP_API_URL}/observations/${observationId}/picture`;
+};
+
 class Observations extends Component {
   render() {
     const { observation, vote } = this.props;
@@ -22,7 +26,7 @@ class Observations extends Component {
             </div>
             <div className="col col-lg-offset-2 col-lg-8">
               <div className="Observations__Picture">
-                <Polaroid img={`${process.env.REACT_APP_API_URL}/observations/${observation.id}/picture`} />
+                <Polaroid img={generateImageUrl(observation.id)} />
               </div>
             </div>
           </div>
