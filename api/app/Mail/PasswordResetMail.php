aebe910437719e258e7047213bddc7d9de1a5e47
Demian Dekoninck <demian@live.be>
2017-07-22 17:58:12 +0000

    Apply fixes from StyleCI

--- api/app/Mail/PasswordResetMail.php
+++ api/app/Mail/PasswordResetMail.php
@@ -3,16 +3,16 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class PasswordResetMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
-    
+
     public $content;
-    
+
     /**
      * Create a new message instance.
      *
