58c540f6e4272dcfe80cc7eeb2ba1c91072e2bd0
Demian Dekoninck <demian@live.be>
2017-07-22 19:58:09 +0200

    Send reset password mail via queue

--- api/app/Mail/PasswordResetMail.php
+++ api/app/Mail/PasswordResetMail.php
@@ -3,17 +3,20 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PasswordResetMail extends Mailable
+class PasswordResetMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
-
+    
+    public $content;
+    
     /**
      * Create a new message instance.
      *
-     * @return void
+     * @param $content
      */
     public function __construct($content)
     {
