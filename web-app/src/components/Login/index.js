cda185ced283c804ca55d0ea9e657845ae01661b
Demian Dekoninck <demian@live.be>
2017-07-24 16:49:35 +0200

    use GuestMode component in Login component

--- web-app/src/components/Login/index.js
+++ web-app/src/components/Login/index.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 
 import Title from '../Title';
 import Divider from '../Divider';
-import { Form, Input, Button, FacebookButton } from '../Form';
+import GuestMode from '../GuestMode';
+
 import { Errors } from '../Alerts';
+import { Form, Input, Button, FacebookButton } from '../Form';
 
 import api, { BASE_URL } from '../../utils/api';
 import redirect from '../../utils/redirect';
 
-import logo from '../../theme/crest.svg';
 import './Login.css';
 
 class Login extends Component {
@@ -25,7 +26,6 @@ class Login extends Component {
 
   login(body) {
     api.post('/auth', body).then(({ data }) => {
-      // TODO: make an easier jwt token manager
       window.localStorage.setItem('jwt.token', data.token);
       redirect('/');
     })
@@ -44,42 +44,39 @@ class Login extends Component {
     const { isValid, errors } = this.state;
 
     return (
-      <div className="Login">
+      <GuestMode className="SignUp">
         <Title name="Login" />
-        <div className="Login__Wrapper">
-          <img src={logo} alt="CODE9000 crest" className="GuestMode__Logo" />
-          <div className="Login__Form">
-            {errors && <Errors errors={errors} />}
-            <Form
-              onValidationChange={valid => this.setState({ isValid: valid })}
-              onSubmit={data => this.login(data)}
-            >
-              <Input name="email" rules={['required', 'email']} placeholder="Email" icon="email" />
-              <Input name="password" type="password" rules={['required']} placeholder="Password" className="Login__Password" icon="lock" />
+        <div className="Login__Form">
+          {errors && <Errors errors={errors} />}
+          <Form
+            onValidationChange={valid => this.setState({ isValid: valid })}
+            onSubmit={data => this.login(data)}
+          >
+            <Input name="email" rules={['required', 'email']} placeholder="Email" icon="email" />
+            <Input name="password" type="password" rules={['required']} placeholder="Password" className="Login__Password" icon="lock" />
 
-              <div className="Login__ForgotPassword">
-                Forgot password? <Link to="/reset-password">Reset Password</Link>
-              </div>
+            <div className="Login__ForgotPassword">
+              Forgot password? <Link to="/reset-password">Reset Password</Link>
+            </div>
 
-              <div className="Login__LoginButton">
-                <Button disabled={!isValid}>Log in</Button>
-              </div>
+            <div className="Login__LoginButton">
+              <Button disabled={!isValid}>Log in</Button>
+            </div>
 
-              <Divider text="or" />
+            <Divider text="or" />
 
-              <div className="Login__LoginButton">
-                <FacebookButton onClick={(e) => this.facebookLogin(e)}>
-                  Sign in with Facebook
-                </FacebookButton>
-              </div>
+            <div className="Login__LoginButton">
+              <FacebookButton onClick={(e) => this.facebookLogin(e)}>
+                Sign in with Facebook
+              </FacebookButton>
+            </div>
 
-              <div className="Login__SignUp">
-                Not a member? <Link to="/sign-up">Sign up here!</Link>
-              </div>
-            </Form>
-          </div>
+            <div className="Login__SignUp">
+              Not a member? <Link to="/sign-up">Sign up here!</Link>
+            </div>
+          </Form>
         </div>
-      </div>
+      </GuestMode>
     );
   }
 }
