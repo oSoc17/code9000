b192f8b1acd973f891769ee2b2a0df2969afa4f4
Demian Dekoninck <demian@live.be>
2017-07-26 21:59:57 +0200

    use flex box for the ranking lines

--- web-app/src/components/Ranking/Ranking.js
+++ web-app/src/components/Ranking/Ranking.js
@@ -6,51 +6,41 @@ import crestMenu from '../../theme/icons/crest_menu.svg';
 import './Ranking.css';
 
 export const Ranking = ({ ranking, user }) => {
-
   const userRankIndex = ranking.findIndex((rankingUser) => rankingUser.id === user.id);
-
   const userRank = ranking[userRankIndex];
-
-  console.log(userRank);
 
   return (
     <div className="container Ranking">
-      <div className="Ranking__Line Ranking_Header">
-        <table>
-          <tbody>
-            <tr>
-              <th className="Ranking__Text__Light">Rank</th>
-              <th className="Ranking__Text__Light Ranking__Text__Center">
-                {user.avatar_url === undefined || user.avatar_url === null
-                  ? <img className="Profile_Picture" src={crestMenu} alt="" />
-                  : <img className="Profile_Picture" src={user.avatar_url} alt="" />
-                }
-              </th>
-              <th className="Ranking__Text__Light Ranking__Text__Right">Points</th>
-            </tr>
-            <tr>
-              <td>{userRankIndex + 1}</td>
-              <td className="Ranking__Line__Name Ranking__Text__Center">{user.name}</td>
-              <td className="Ranking__Text__Right">
-                <TransitiveNumber>{userRank.points}</TransitiveNumber>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="Ranking__Header">
+        <div className="Ranking__Header__Rank">
+          <div>Rank</div>
+          <div>{userRankIndex + 1}</div>
+        </div>
+        <div className="Ranking__Header__Profile">
+          {user.avatar_url === undefined || user.avatar_url === null
+            ? <img className="Profile__Header__Picture" src={crestMenu} alt="" />
+            : <img className="Profile__Header__Picture" src={user.avatar_url} alt="" />
+          }
+          <div>{user.name}</div>
+        </div>
+        <div className="Ranking__Header__Points">
+          <div>Points</div>
+          <div>{userRank.points}</div>
+        </div>
       </div>
-      <table>
-        <tbody>
-          {ranking.map((rank, index) => (
-            <tr key={rank.id} className="Ranking__Line">
-              <td>{index + 1}</td>
-              <td className="Ranking__Line__Name">{rank.name}</td>
-              <td>
-                <TransitiveNumber>{rank.points}</TransitiveNumber>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {ranking.map((rank, index) => (
+        <div key={rank.id} className="Ranking__Line">
+          <div className="Ranking__Line__Position">
+            {index + 1}
+          </div>
+          <div className="Ranking__Line__Name">
+            {rank.name}
+          </div>
+          <div className="Ranking__Line__Points">
+            <TransitiveNumber>{rank.points}</TransitiveNumber>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
