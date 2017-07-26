fb5220532d8cfc5b742aff859c1b4d3cd9a7a41d
Demian Dekoninck <demian@live.be>
2017-07-26 12:27:19 +0200

    make polaroids on onboarding not zoomable

--- web-app/src/components/Polaroid/index.js
+++ web-app/src/components/Polaroid/index.js
@@ -20,25 +20,33 @@ class Polaroid extends Component {
   }
 
   render() {
-    const { img, toggle } = this.props;
+    const { img, toggle, zoomable } = this.props;
     const { showModal } = this.state;
 
     return (
       <div className={classNames('Polaroid', toggle && 'Polaroid__Animation')} >
         <img src={img} alt="" className="Polaroid__Image" />
-        <img onClick={() => this.showModal(true)} src={zoomIcon} alt="Zoom icon" className="Polaroid__ZoomIcon" />
+        {zoomable && (
+          <div>
+            <img onClick={() => this.showModal(true)} src={zoomIcon} alt="Zoom icon" className="Polaroid__ZoomIcon" />
 
-        <ReactModal
-          isOpen={showModal}
-          contentLabel="Big modal showing the polaroid in big size."
-        >
-          <img src={img} alt="" onClick={() => this.showModal(false)} />
-        </ReactModal>
+            <ReactModal
+              isOpen={showModal}
+              contentLabel="Big modal showing the polaroid in big size."
+            >
+              <img src={img} alt="" onClick={() => this.showModal(false)} />
+            </ReactModal>
+          </div>
+        )}
 
         <div className="Polaroid__Bottom" />
       </div>
     );
   }
 }
+
+Polaroid.defaultProps = {
+  zoomable: true,
+};
 
 export default Polaroid;
