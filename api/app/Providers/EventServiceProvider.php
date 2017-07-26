9610ec6a46b7d1b764af06a1e5768098a52949aa
Demian Dekoninck <demian@live.be>
2017-07-26 15:52:26 +0200

    register the listeners

--- api/app/Providers/EventServiceProvider.php
+++ api/app/Providers/EventServiceProvider.php
@@ -13,6 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
+        'App\Events\ObservationUploaded' => [
+            'App\Listeners\SendObservationToFacebook',
+        ],
         'App\Events\ObservationIsValid' => [
             'App\Listeners\SendObservationImgur',
         ],
