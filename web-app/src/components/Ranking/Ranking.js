cfd5d7aba4b9977b59a7afe412d475b41668acec
Diego De Wilde <diego@dewilde.it>
2017-07-26 16:55:58 +0200

    implement design without profile values

--- web-app/src/components/Ranking/Ranking.js
+++ web-app/src/components/Ranking/Ranking.js
@@ -5,28 +5,37 @@ import './Ranking.css';
 
 export const Ranking = ({ ranking }) => (
   <div className="container Ranking">
-    <div className="row">
-      <div className="col-lg-12">
-        <h1>Ranking</h1>
-      </div>
-      <div className="col-lg-12">
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th><th>Points</th>
-            </tr>
-            {ranking.map((rank) => (
-              <tr key={rank.id}>
-                <td>{rank.name}</td>
-                <td>
-                  <TransitiveNumber>{rank.points}</TransitiveNumber>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className="Ranking__Line Ranking_Header">
+      <table>
+        <tbody>
+          <tr>
+            <th className="Ranking__Text__Light">Rank</th>
+            <th className="Ranking__Text__Light Ranking__Text__Center"><img className="Profile_Picture" src="#" alt="Profile picture" /></th>
+            <th className="Ranking__Text__Light Ranking__Text__Right">Points</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td className="Ranking__Line__Name Ranking__Text__Center">Name</td>
+            <td className="Ranking__Text__Right">
+              <TransitiveNumber>100</TransitiveNumber>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    <table>
+      <tbody>
+        {ranking.map((rank, index) => (
+          <tr key={rank.id} className="Ranking__Line">
+            <td>{index+1}</td>
+            <td className="Ranking__Line__Name">{rank.name}</td>
+            <td>
+              <TransitiveNumber>{rank.points}</TransitiveNumber>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </div>
 );
 
